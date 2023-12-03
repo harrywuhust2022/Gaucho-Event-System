b7aec97fe30caad488c322e31aa11b45318bd3ec
kdzuo <kdz1062735832@gmail.com>
2023-12-02 19:33:40 -0800

    add frontend authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,26 @@ class CommentsController < ApplicationController
     @user = User.find(params[:user_id])
     @event = @user.created_events.find(params[:event_id])
     @comment = @event.comments.build(comment_params)
-    @admin = User.find_by(username: 'admin')
-    if @admin.nil?
-      @admin = User.new
-      @admin.username = "admin"
-      @admin.password = "admin"
-      @admin.email = "admin@email.com"
-      unless @admin.save
-        puts 'no admin or save fails'
-      end
+
+    # @admin = User.find_by(username: 'admin')
+    # if @admin.nil?
+    #   @admin = User.new
+    #   @admin.username = "admin"
+    #   @admin.password = "admin"
+    #   @admin.email = "admin@email.com"
+    #   unless @admin.save
+    #     puts 'no admin or save fails'
+    #   end
+    # end
+    # @comment.user = @admin
+    if current_user.nil?
+      redirect_to '/login'
+    else
+      @comment.user = current_user
+      @comment.save
+      redirect_to user_event_path(@user, @event)
     end
-    @comment.user = @admin
-    @comment.save
-    redirect_to user_event_path(@user, @event)
+
 
   end
   private
